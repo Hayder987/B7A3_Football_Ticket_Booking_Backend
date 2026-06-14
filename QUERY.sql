043f53035f9355d2a6087efd525f34a3381f8820
Hayder Ali <hayderbd4290@gmail.com>
2026-06-14 17:54:43 +0600

    solve problem 2 search using ilike

--- QUERY.sql
+++ QUERY.sql
@@ -67,6 +67,7 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 -------------------------------------Query Part ----------------------------------------------------
 
 --Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
 SELECT
   match_id,
   fixture,
@@ -76,6 +77,18 @@ FROM
 WHERE
   tournament_category = 'Champions League'
   AND match_status = 'Available';
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT
+    user_id,
+    full_name,
+    email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+OR full_name ILIKE '%Haque%';
+
 
 
 
